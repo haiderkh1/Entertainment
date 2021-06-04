620c1e660fc60d1f16aa5b690ab6ff5a14dedc78
Haider <haideryouxd4@gmail.com>
2021-06-04 13:04:11 +0500

    tv-shows

---
+++ app.js
@@ -0,0 +1,81 @@
+$(document).ready(() => {
+  $("#hamburger-menu").click(() => {
+    $("#hamburger-menu").toggleClass("active");
+    $("#nav-menu").toggleClass("active");
+  });
+
+  // setting owl carousel
+
+  let navText = [
+    "<i class='bx bx-chevron-left'></i>",
+    "<i class='bx bx-chevron-right'></i>",
+  ];
+
+  $("#hero-carousel").owlCarousel({
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: navText,
+    autoplay: true,
+    smartSpeed: 1500,
+    autoplayHoverPause: false,
+  });
+
+  $("#top-movies-slide").owlCarousel({
+    items: 2,
+    dots: false,
+    loop: true,
+    autoplay: 1000,
+    smartSpeed: 1000,
+    autoplayHoverPause: false,
+    responsive: {
+      500: {
+        items: 3,
+      },
+      1280: {
+        items: 4,
+      },
+      1600: {
+        items: 6,
+      },
+    },
+  });
+
+  $(".movies-slide").owlCarousel({
+    items: 2,
+    dots: false,
+    nav: true,
+    navText: navText,
+    smartSpeed: 1000,
+    margin: 15,
+    responsive: {
+      500: {
+        items: 2,
+      },
+      1280: {
+        items: 4,
+      },
+      1600: {
+        items: 6,
+      },
+    },
+  });
+
+  // back to top button
+
+  var btn = $("#button");
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 2000) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+});
